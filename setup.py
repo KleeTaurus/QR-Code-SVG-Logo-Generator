89fb1cf73cb7625eedc65151e16fb6b6dfb64ecc
Kevin Lee <klee.taurus@gmail.com>
2015-11-17 11:47:38 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name            = 'qrcodesvglogo',
+    version         = '1.0.0',
+    packages        = [''],
+    author          = 'klee',
+    author_email    = 'klee.taurus@gmail.com',
+    url             = 'https://github.com/KleeTaurus/QR-Code-SVG-Logo-Generator',
+    description     = 'Generate QR codes with your logo in the middle.',
+)
